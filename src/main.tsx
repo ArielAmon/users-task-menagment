78b4c4b3cab9e21a86e8552678e5c6b52d9f3825
Ariel Amon <arielamon1445@gmail.com>
2024-09-07 18:51:28 +0300

    remove double rendering because router Config

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { UserProvider } from "./contexts/UserContext.tsx";
-import ErrorPage from "./error-page.tsx";
+// import ErrorPage from "./error-page.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//   },
+// ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <UserProvider>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
       <App />
     </UserProvider>
   </React.StrictMode>
